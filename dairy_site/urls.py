117b199e73bc3a275e038b5b40895267485b0636
dilip\ jat <dilipjat98@gmail.com>
2019-09-30 22:59:28 +0530

    login working and initialized customer app

---
+++ dairy_site/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,include
+from django.contrib import admin
+from . import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+]
+if settings.DEBUG:
+    urlpatterns = urlpatterns +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
