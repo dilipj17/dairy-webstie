14bbf67fd3bacae545a219fd08b2d7410a62300b
dilip jat <dilip.j17@iiits.in>
2019-11-08 00:38:12 +0530

    on Ledger customer name and date added

--- static/customer/balanceupdate.js
+++ static/customer/balanceupdate.js
@@ -17,3 +17,20 @@ for (var i = 1;; i++) {
     break;
   }
 }
+
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
+
+document.getElementById('date-tab').innerHTML = formatDate(new Date())
