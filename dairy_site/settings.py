0bc0767af3f018b442f2ca5c6053bcc02b55d1fe
dilip jat <dilip.j17@iiits.in>
2019-11-24 23:07:13 +0530

    some changes

--- dairy_site/settings.py
+++ dairy_site/settings.py
@@ -9,11 +9,11 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = False
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=os.environ['DATABASE_URL']
     )
 }
 
